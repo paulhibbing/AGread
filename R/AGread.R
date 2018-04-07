05370bd6fdab3224abbe00c570976581c0ee94fc
Paul R. Hibbing <paulhibbing@gmail.com>
2018-04-06 22:54:48 -0400

    Initial commit

---
+++ R/AGread.R
@@ -0,0 +1,14 @@
+#' Read Accelerometer Files from ActiGraph Accelerometers
+#'
+#' This provides support for reading ActiGraph files of various modes into R.
+#' For more information see:
+#' \url{https://actigraph.desk.com/customer/en/portal/articles/2515800-what-do-the-different-mode-numbers-mean-in-a-csv-or-dat-file-}
+#'
+#' @section Core functions:
+#'
+#' @examples
+#'
+#'
+#' @docType package
+#' @name AGread
+NULL
---
+++ R/zzz.R
@@ -0,0 +1,3 @@
+.onAttach <- function(libname, pkgname) {
+  packageStartupMessage("package 'AGread' was built under R version 3.4.3")
+}
