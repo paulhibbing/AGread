53ffaa58137583ff5e3d43ff54641a5be309891c
Paul Hibbing <paulhibbing@gmail.com>
2019-02-13 16:45:10 -0500

    Add tests for read_ag_RAW and an example gt3x file

---
+++ tests/testthat/test_RAW.R
@@ -0,0 +1,33 @@
+context("RAW data processing")
+library(AGread)
+devtools::load_all()
+
+## MANUAL LEGACY TEST (verified 2/13/19)
+# AG_legacy <- readRDS("data-raw/RAW_legacy.rds")
+# AG <- read_AG_raw(
+#   system.file(
+#     "extdata", "TestID_LeftWrist_RAW.csv", package = "AGread"
+#   )
+#   )[ ,-2]
+# > all.equal(AG, AG_legacy)
+# [1] TRUE
+
+testthat::test_that("File reading produces expected output", {
+  testthat::expect_equal_to_reference(
+    read_AG_raw(
+      system.file(
+        "extdata", "TestID_LeftWrist_RAW.csv", package = "AGread"
+      )
+    )[ ,-2],
+    "RAW_read_cache_1.rds"
+  )
+  testthat::expect_equal_to_reference(
+    read_AG_raw(
+      system.file(
+        "extdata", "TestID_LeftWrist_RAW.csv", package = "AGread"
+      ),
+      5
+    )[ ,-2],
+    "RAW_read_cache_5.rds"
+  )
+})
