060689014bebb6b1a62c857cf1b839becc5125d5
Paul R. Hibbing <paulhibbing@gmail.com>
2019-04-29 16:09:59 -0400

    Move get_duration to PAutilities and deprecate it

---
+++ R/AGread-deprecated.R
@@ -0,0 +1,8 @@
+#' @title Deprecated functions in package \pkg{AGread}.
+#' @description These are functions that are deprecated because they have been
+#'   moved elsewhere, primarily to the PAutilities package. Use
+#'   \code{help("-deprecated")} to access documentation for deprecated
+#'   functions.
+#' @name AGread-deprecated
+#' @keywords internal
+NULL
