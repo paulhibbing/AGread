990be0c023abb4cff41084763c62a5e22a68b6ce
Paul R. Hibbing <paulhibbing@gmail.com>
2019-09-12 02:05:19 -0400

    Update documentation; gives warnings for outdated usage of inheritParams

--- R/parse_packet_set_generic_&_simple_methods.R
+++ R/parse_packet_set_generic_&_simple_methods.R
@@ -1,60 +1,68 @@
 # Generic function---------------------------------------------------------
 
-  parse_packet_set <- function(
-    set, log, tz = "UTC", verbose = FALSE,
-    give_timestamp = TRUE, ...
-  ) {
+#' Parse all packets of a given type
+#'
+#' @param set the set of record headers corresponding to each packet
+#' @param log the raw data from \code{log.bin}
+#' @inheritParams read_gt3x
+#' @param ... further arguments passed to methods
+#'
+#' @keywords internal
+parse_packet_set <- function(
+  set, log, tz = "UTC", verbose = FALSE,
+  give_timestamp = TRUE, ...
+) {
 
-    if (verbose) packet_print("startup", class(set)[1])
-    UseMethod("parse_packet_set", set)
+  if (verbose) packet_print("startup", class(set)[1])
+  UseMethod("parse_packet_set", set)
 
-  }
+}
 
 # Simple methods ----------------------------------------------------------
 
-  #' @rdname parse_packet_set
-  #' @export
-  parse_packet_set.default <- function(
-    set, log, tz = "UTC", verbose = FALSE,
-    give_timestamp = TRUE, ...
-  ) {
+#' @rdname parse_packet_set
+#' @export
+parse_packet_set.default <- function(
+  set, log, tz = "UTC", verbose = FALSE,
+  give_timestamp = TRUE, ...
+) {
 
-    warning(paste(
-      "No method exists yet for parsing",
-      class(set)[1], "packets -- returning NULL."
-    ))
+  warning(paste(
+    "No method exists yet for parsing",
+    class(set)[1], "packets -- returning NULL."
+  ))
 
-    return(NULL)
+  return(NULL)
 
-  }
+}
 
-  #' @rdname parse_packet_set
-  #' @export
-  parse_packet_set.PARAMETERS <- function(
-    set, log, tz = "UTC", verbose = FALSE,
-    give_timestamp = TRUE, ...
-  ) {
+#' @rdname parse_packet_set
+#' @export
+parse_packet_set.PARAMETERS <- function(
+  set, log, tz = "UTC", verbose = FALSE,
+  give_timestamp = TRUE, ...
+) {
 
-    payload <- setup_payload(set, log) %>%
-      split(cumsum(seq(.) %% 8 == 1)) %>%
-      {do.call(rbind, lapply(., process_parameters, tz = tz))} %>%
-      {.[.$Label != "UNUSED KEY", ]} %>%
-      {stats::setNames(as.list(.$value), .$Label)}
+  payload <- setup_payload(set, log) %>%
+    split(cumsum(seq(.) %% 8 == 1)) %>%
+    {do.call(rbind, lapply(., process_parameters, tz = tz))} %>%
+    {.[.$Label != "UNUSED KEY", ]} %>%
+    {stats::setNames(as.list(.$value), .$Label)}
 
-    value <- list(
-      Type = class(set)[1],
-      Timestamp = lubridate::force_tz(
-        set$timestamp, tz
-      ),
-      Payload_Size = set$payload_size,
-      Payload = payload,
-      Checksum = "OK"
-    )
+  value <- list(
+    Type = class(set)[1],
+    Timestamp = lubridate::force_tz(
+      set$timestamp, tz
+    ),
+    Payload_Size = set$payload_size,
+    Payload = payload,
+    Checksum = "OK"
+  )
 
-    if (!give_timestamp) value$Timestamp <- NULL
+  if (!give_timestamp) value$Timestamp <- NULL
 
-    if (verbose) packet_print("cleanup", class(set)[1])
+  if (verbose) packet_print("cleanup", class(set)[1])
 
-    structure(value, class = class(set)[1])
+  structure(value, class = class(set)[1])
 
-  }
+}
