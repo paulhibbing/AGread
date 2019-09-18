f0d9c8973fac151d09fdb18a336b18914ea0454e
Paul R. Hibbing <paulhibbing@gmail.com>
2019-09-17 22:01:18 -0400

    Specify build under R 3.5.0

--- R/zzz.R
+++ R/zzz.R
@@ -1,5 +1,5 @@
 if(getRversion() >= "2.15.1") utils::globalVariables(".")
 
 .onAttach <- function(libname, pkgname) {
-  packageStartupMessage("package 'AGread' was built under R version 3.5.2")
+  packageStartupMessage("package 'AGread' was built under R version 3.5.0")
 }
