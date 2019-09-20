79aa703e2dd331ce60d9c50dc456d134988195ec
Paul Hibbing <paulhibbing@gmail.com>
2019-09-20 12:00:21 -0400

    Resolve header/function type mismatch for payload_parse_sensor_data_25C

--- src/imu_payload.h
+++ src/imu_payload.h
@@ -2,7 +2,7 @@
 #define IMU_PAYLOAD_H
 
 #include <Rcpp.h>
-Rcpp::List payload_parse_sensor_data_25C(
+Rcpp::DataFrame payload_parse_sensor_data_25C(
     Rcpp::RawVector payload, Rcpp::DataFrame info,
     int id, int samp_rate
 );
