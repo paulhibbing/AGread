c3a6c876ea0d27a6df7e8d8a2e8f09b745dbde6a
Paul Hibbing <paulhibbing@gmail.com>
2019-10-30 14:10:27 -0400

    Remove excess internal functions and restructure storage of deprecated functions/doc

--- R/AGread-deprecated.R
+++ R/AGread-deprecated.R
@@ -1,3 +1,5 @@
+# Documentation -----------------------------------------------------------
+
 #' @title Deprecated functions in package \pkg{AGread}.
 #' @description These are functions that are deprecated because they have been
 #'   moved elsewhere, primarily to the PAutilities package. Use
@@ -6,6 +8,30 @@
 #' @name AGread-deprecated
 #' @keywords internal
 NULL
+
+#' Provide the run time of processing
+#'
+#' @param timer a proc_time object giving the initial time
+#'
+#' @examples
+#' timer <- proc.time()
+#' Sys.sleep(2.2)
+#' \donttest{
+#'   # Gives warning about deprecation
+#'   # get_duration(timer)
+#'
+#'   # Instead use
+#'   PAutilities::get_duration(timer)
+#' }
+#'
+#'
+#' @name get_duration-deprecated
+#' @usage get_duration(timer)
+#' @seealso \code{\link{AGread-deprecated}}
+#' @keywords internal
+NULL
+
+# Functions ---------------------------------------------------------------
 
 #' @rdname AGread-deprecated
 #' @section \code{get_duration}:
@@ -20,4 +46,5 @@ get_duration <- function(timer) {
     digits = 1,
     nsmall = 1
   )
+
 }
