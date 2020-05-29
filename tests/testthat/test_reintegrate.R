b1a877022a5645fa15701e4ab796d071d62c098c
Paul R. Hibbing <paulhibbing@gmail.com>
2020-05-29 00:12:55 -0400

    Demonstrate alignment between revised reintegration and the test cache (accounting for known issues)

--- tests/testthat/test_reintegrate.R
+++ tests/testthat/test_reintegrate.R
@@ -1,7 +1,7 @@
 context("Reintegration")
 library(AGread)
 
-testthat::test_that("Rcpp reintegration produces expected output", {
+testthat::test_that("Reintegration produces expected output", {
 
   test_file <- system.file(
     "extdata", "example1sec.csv", package = "AGread"
@@ -10,39 +10,15 @@ testthat::test_that("Rcpp reintegration produces expected output", {
   ag <- read_AG_counts(test_file, skip = 11)
 
   forward5 <- reintegrate(ag, 5, "Timestamp", "forwards")
+  row.names(forward5) <- as.character(row.names(forward5))
   forward60 <- reintegrate(forward5, 60, "Timestamp", "forwards")
+  row.names(forward60) <- as.character(row.names(forward60))
   backward5 <- reintegrate(ag, 5, "Timestamp", "backwards")
+  row.names(backward5) <- as.character(row.names(backward5))
   backward60 <- reintegrate(backward5, 60, "Timestamp", "backwards")
+  backward60 <- backward60[-1, ]
+  row.names(backward60) <- seq(nrow(backward60))
 
-  testthat::expect_equal_to_reference(forward5, "forward5.rds")
-  testthat::expect_equal_to_reference(forward60, "forward60.rds")
-  testthat::expect_equal_to_reference(backward5, "backward5.rds")
-  testthat::expect_equal_to_reference(backward60, "backward60.rds")
-
-})
-
-testthat::test_that("Legacy reintegration produces expected output", {
-
-  test_file <- system.file(
-    "extdata", "example1sec.csv", package = "AGread"
-  )
-
-  ag <- read_AG_counts(test_file, skip = 11)
-
-  forward5 <- reintegrate(
-    ag, 5, "Timestamp", "forwards", method = "legacy"
-  )
-  forward60 <- reintegrate(
-    forward5, 60, "Timestamp", "forwards", method = "legacy"
-  )
-  backward5 <- reintegrate(
-    ag, 5, "Timestamp", "backwards", method = "legacy"
-  )
-  backward60 <- reintegrate(
-    backward5, 60, "Timestamp", "backwards", method = "legacy"
-  )
-
-  ## (Test against same references as legacy)
   testthat::expect_equal_to_reference(forward5, "forward5.rds")
   testthat::expect_equal_to_reference(forward60, "forward60.rds")
   testthat::expect_equal_to_reference(backward5, "backward5.rds")
